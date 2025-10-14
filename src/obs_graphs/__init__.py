9048f86fa9bad3f73ebf140cc9bc4edc05360b19
akitorahayashi <hayashi20030703@gmail.com>
2025-10-14 18:15:48 +0900

    test: Update e2e tests to use new DI architecture instead of container pattern

--- src/obs_graphs/__init__.py
+++ src/obs_graphs/__init__.py
@@ -2,11 +2,4 @@
 
 from .main import app
 
-
-def get_container():
-    from .container import get_container as _get_container
-
-    return _get_container()
-
-
-__all__ = ["app", "get_container"]
+__all__ = ["app"]
